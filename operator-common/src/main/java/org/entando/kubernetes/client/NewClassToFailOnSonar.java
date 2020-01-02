e59a76a3e06cf9a1e8348a6691f9750436182df2
Ampie Barnard <a.barnard@entando.com>
2020-01-02 14:24:46 +0200

    Deep fetch for PRs

---
+++ operator-common/src/main/java/org/entando/kubernetes/client/NewClassToFailOnSonar.java
@@ -0,0 +1,30 @@
+package org.entando.kubernetes.client;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NewClassToFailOnSonar {
+    private Logger logger=Logger.getLogger(NewClassToFailOnSonar.class.getSimpleName());
+    public void doNothing(){
+        logger.log(Level.SEVERE,"Hello world 1");
+        logger.log(Level.SEVERE,"Hello world 2");
+        logger.log(Level.SEVERE,"Hello world 3");
+        logger.log(Level.SEVERE,"Hello world 4");
+        logger.log(Level.SEVERE,"Hello world 5");
+        logger.log(Level.SEVERE,"Hello world 6");
+        logger.log(Level.SEVERE,"Hello world 7");
+        logger.log(Level.SEVERE,"Hello world 8");
+        logger.log(Level.SEVERE,"Hello world 9");
+        logger.log(Level.SEVERE,"Hello world 10");
+        logger.log(Level.SEVERE,"Hello world 11");
+        logger.log(Level.SEVERE,"Hello world 12");
+        logger.log(Level.SEVERE,"Hello world 13");
+        logger.log(Level.SEVERE,"Hello world 14");
+        logger.log(Level.SEVERE,"Hello world 15");
+        logger.log(Level.SEVERE,"Hello world 16");
+        logger.log(Level.SEVERE,"Hello world 17");
+        logger.log(Level.SEVERE,"Hello world 18");
+        logger.log(Level.SEVERE,"Hello world 19");
+        logger.log(Level.SEVERE,"Hello world 20");
+    }
+}
--- operator-common/src/main/java/org/entando/kubernetes/client/PodWatcherHolder.java
+++
@@ -1,6 +0,0 @@
-package org.entando.kubernetes.client;
-
-public interface PodWatcherHolder {
-
-    void current(PodWatcher w);
-}
