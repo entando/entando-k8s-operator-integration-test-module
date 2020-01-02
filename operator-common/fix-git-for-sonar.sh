2fc0b201a38f557f069e71981f8ddd3ddcaf7f28
Ampie Barnard <a.barnard@entando.com>
2020-01-02 16:45:31 +0200

    Still fixing Git/SOnar issue

--- operator-common/fix-git-for-sonar.sh
+++ operator-common/fix-git-for-sonar.sh
@@ -1,4 +1,4 @@
 git fetch --all
-git checkout origin/$(git name-rev ${PULL_PULL_SHA}| awk '{print $2}') --track
+git checkout -b PR-8 ${PULL_PULL_SHA}
 git branch master -D
 git fetch origin master:master
