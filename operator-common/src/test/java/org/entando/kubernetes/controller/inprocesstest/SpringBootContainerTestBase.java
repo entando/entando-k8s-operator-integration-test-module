6ac0da7a0c0421aba3baaff2d993913fd002483a
Ampie Barnard <a.barnard@entando.com>
2020-01-31 11:49:14 +0200

    Made more tests use auto-generated names

--- operator-common/src/test/java/org/entando/kubernetes/controller/inprocesstest/SpringBootContainerTestBase.java
+++ operator-common/src/test/java/org/entando/kubernetes/controller/inprocesstest/SpringBootContainerTestBase.java
@@ -4,8 +4,6 @@ import static org.awaitility.Awaitility.await;
 import static org.entando.kubernetes.controller.KubeUtils.standardIngressName;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.lenient;
 
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
@@ -13,7 +11,6 @@ import io.fabric8.kubernetes.api.model.extensions.Ingress;
 import io.fabric8.kubernetes.client.Watcher.Action;
 import io.quarkus.runtime.StartupEvent;
 import java.util.concurrent.TimeUnit;
-import org.entando.kubernetes.controller.KeycloakClientConfig;
 import org.entando.kubernetes.controller.KeycloakConnectionConfig;
 import org.entando.kubernetes.controller.KubeUtils;
 import org.entando.kubernetes.controller.ServiceDeploymentResult;
@@ -24,7 +21,6 @@ import org.entando.kubernetes.controller.common.examples.springboot.SpringBootDe
 import org.entando.kubernetes.controller.creators.KeycloakClientCreator;
 import org.entando.kubernetes.controller.database.DatabaseServiceResult;
 import org.entando.kubernetes.controller.integrationtest.support.EntandoOperatorTestConfig;
-import org.entando.kubernetes.controller.k8sclient.SimpleK8SClient;
 import org.entando.kubernetes.controller.spi.Deployable;
 import org.entando.kubernetes.controller.spi.SpringBootDeployableContainer;
 import org.entando.kubernetes.controller.test.support.FluentTraversals;
@@ -38,25 +34,19 @@ import org.entando.kubernetes.model.plugin.EntandoPlugin;
 import org.entando.kubernetes.model.plugin.EntandoPluginBuilder;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 public abstract class SpringBootContainerTestBase implements InProcessTestUtil, FluentTraversals, PodBehavior, VariableReferenceAssertions {
 
     public static final String SAMPLE_NAMESPACE = EntandoOperatorTestConfig.calculateNameSpace("sample-namespace");
     public static final String SAMPLE_NAME = EntandoOperatorTestConfig.calculateName("sample-name");
-    public static final String OTHER_NAMESPACE = EntandoOperatorTestConfig.calculateNameSpace("other-namespace");
-    public static final String OTHER_NAME = EntandoOperatorTestConfig.calculateName("other-name");
-    protected SimpleK8SClient k8sClient;
+    public static final String SAMPLE_NAME_DB = KubeUtils.snakeCaseOf(SAMPLE_NAME + "_db");
     EntandoPlugin plugin1 = buildPlugin(SAMPLE_NAMESPACE, SAMPLE_NAME);
-    SimpleKeycloakClient mock = Mockito.mock(SimpleKeycloakClient.class);
     private SampleController controller;
 
     @Test
     public void testBasicDeployment() {
         //Given I have a controller that processes EntandoPlugins
-        lenient().when(mock.prepareClientAndReturnSecret(any(KeycloakClientConfig.class))).thenReturn("ASDFASDFASDfa");
-        this.k8sClient = getClient();
-        controller = new SampleController<EntandoPlugin>(k8sClient, mock) {
+        controller = new SampleController<EntandoPlugin>(getClient(), getKeycloakClient()) {
             @Override
             protected Deployable<ServiceDeploymentResult> createDeployable(EntandoPlugin newEntandoPlugin,
                     DatabaseServiceResult databaseServiceResult,
@@ -65,34 +55,36 @@ public abstract class SpringBootContainerTestBase implements InProcessTestUtil, 
             }
         };
         //And I have prepared the Standard KeycloakAdminSecert
-        k8sClient.secrets().overwriteControllerSecret(buildKeycloakSecret());
+        getClient().secrets().overwriteControllerSecret(buildKeycloakSecret());
         //And we can observe the pod lifecycle
         emulatePodWaitingBehaviour(plugin1, plugin1.getMetadata().getName());
         //When I create a new EntandoPlugin
         onAdd(plugin1);
 
         await().ignoreExceptions().atMost(2, TimeUnit.MINUTES).until(() ->
-                k8sClient.entandoResources()
+                getClient().entandoResources()
                         .load(plugin1.getClass(), plugin1.getMetadata().getNamespace(), plugin1.getMetadata().getName())
                         .getStatus()
                         .getEntandoDeploymentPhase() == EntandoDeploymentPhase.SUCCESSFUL);
         //Then I expect a server deployment
-        Deployment serverDeployment = k8sClient.deployments()
+        Deployment serverDeployment = getClient().deployments()
                 .loadDeployment(plugin1, SAMPLE_NAME + "-" + KubeUtils.DEFAULT_SERVER_QUALIFIER + "-deployment");
         assertThat(serverDeployment.getSpec().getTemplate().getSpec().getContainers().size(), Matchers.is(1));
-        verifyThatAllVariablesAreMapped(plugin1, k8sClient, serverDeployment);
-        verifyThatAllVolumesAreMapped(plugin1, k8sClient, serverDeployment);
+        verifyThatAllVariablesAreMapped(plugin1, getClient(), serverDeployment);
+        verifyThatAllVolumesAreMapped(plugin1, getClient(), serverDeployment);
         verifySpringDatasource(serverDeployment);
         verifySpringOidc(serverDeployment);
         //Then  a db deployment
-        Deployment dbDeployment = k8sClient.deployments().loadDeployment(plugin1, SAMPLE_NAME + "-db-deployment");
+        Deployment dbDeployment = getClient().deployments().loadDeployment(plugin1, SAMPLE_NAME + "-db-deployment");
         assertThat(dbDeployment.getSpec().getTemplate().getSpec().getContainers().size(), is(1));
 
         //And I an ingress paths
-        Ingress ingress = k8sClient.ingresses().loadIngress(plugin1.getMetadata().getNamespace(), standardIngressName(plugin1));
+        Ingress ingress = getClient().ingresses().loadIngress(plugin1.getMetadata().getNamespace(), standardIngressName(plugin1));
         assertThat(theHttpPath(SampleSpringBootDeployableContainer.MY_WEB_CONTEXT).on(ingress).getBackend().getServicePort().getIntVal(),
                 Matchers.is(8080));
     }
+
+    protected abstract SimpleKeycloakClient getKeycloakClient();
 
     public void verifySpringOidc(Deployment serverDeployment) {
         assertThat(theVariableNamed(SpringBootDeployableContainer.SPRING_SECURITY_OAUTH_2_CLIENT_PROVIDER_OIDC_ISSUER_URI)
@@ -120,7 +112,7 @@ public abstract class SpringBootContainerTestBase implements InProcessTestUtil, 
                 .on(thePrimaryContainerOn(serverDeployment)).getSecretKeyRef().getName(), is(SAMPLE_NAME + "-serverdb-secret"));
         assertThat(theVariableNamed(SpringBootDeployableContainer.SPRING_DATASOURCE_URL)
                 .on(thePrimaryContainerOn(serverDeployment)), is(
-                "jdbc:postgresql://sample-name-db-service.sample-namespace.svc.cluster.local:5432/sample_name_db"));
+                "jdbc:postgresql://" + SAMPLE_NAME + "-db-service." + SAMPLE_NAMESPACE + ".svc.cluster.local:5432/" + SAMPLE_NAME_DB));
     }
 
     private EntandoPlugin buildPlugin(String sampleNamespace, String sampleName) {
@@ -159,7 +151,7 @@ public abstract class SpringBootContainerTestBase implements InProcessTestUtil, 
     @SuppressWarnings("unchecked")
     public <T extends EntandoBaseCustomResource> void onAdd(T resource) {
         new Thread(() -> {
-            T createResource = k8sClient.entandoResources().putEntandoCustomResource(resource);
+            T createResource = getClient().entandoResources().putEntandoCustomResource(resource);
             System.setProperty(KubeUtils.ENTANDO_RESOURCE_ACTION, Action.ADDED.name());
             System.setProperty(KubeUtils.ENTANDO_RESOURCE_NAMESPACE, createResource.getMetadata().getNamespace());
             System.setProperty(KubeUtils.ENTANDO_RESOURCE_NAME, createResource.getMetadata().getName());
