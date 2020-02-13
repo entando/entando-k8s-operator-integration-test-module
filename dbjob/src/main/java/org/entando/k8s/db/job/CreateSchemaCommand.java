357310c7b7de75b1f6bb075fa8dc3fbe60e0fb73
Ampie Barnard <a.barnard@entando.com>
2020-02-13 13:50:59 +0200

    Now assuming services in jx namespace

--- dbjob/src/main/java/org/entando/k8s/db/job/CreateSchemaCommand.java
+++ dbjob/src/main/java/org/entando/k8s/db/job/CreateSchemaCommand.java
@@ -1,24 +1,39 @@
 package org.entando.k8s.db.job;
 
+import io.quarkus.runtime.StartupEvent;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 
 public class CreateSchemaCommand {
 
+    private static final Logger logger = Logger.getLogger(CreateSchemaCommand.class.getName());
     private DatabaseAdminConfig databaseAdminConfig;
+    private int status = 0;
 
+    @Inject
     public CreateSchemaCommand(DatabaseAdminConfig databaseAdminConfig) {
-
         this.databaseAdminConfig = databaseAdminConfig;
     }
 
-    public static void main(String[] args) throws SQLException {
-        new CreateSchemaCommand(new PropertiesBasedDatabaseAdminConfig(System.getenv())).execute();
+    public void onStart(@Observes StartupEvent startupEvent) {
+        try {
+            execute();
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Schema creation failed.", e);
+            status = -1;
+        } finally {
+            new Thread(() -> {
+                System.exit(status);
+            }).start();
+        }
     }
 
     public void execute() throws SQLException {
-
         DatabaseDialect dialect = DatabaseDialect.resolveFor(databaseAdminConfig.getDatabaseVendor());
         if (!dialect.schemaExists(databaseAdminConfig)) {
             try (Connection connection = dialect.connect(this.databaseAdminConfig)) {
