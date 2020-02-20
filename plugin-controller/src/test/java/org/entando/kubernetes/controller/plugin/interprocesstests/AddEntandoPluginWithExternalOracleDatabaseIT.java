6433698b493a2dbd0d5dae5a5b721dd8b1a8de6b
Ampie Barnard <a.barnard@entando.com>
2020-02-20 17:40:42 +0200

    Reenabled the oracle test

--- plugin-controller/src/test/java/org/entando/kubernetes/controller/plugin/interprocesstests/AddEntandoPluginWithExternalOracleDatabaseIT.java
+++ plugin-controller/src/test/java/org/entando/kubernetes/controller/plugin/interprocesstests/AddEntandoPluginWithExternalOracleDatabaseIT.java
@@ -29,7 +29,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
-@Tags({@Tag("end-to-end"), @Tag("inter-processk"), @Tag("oracle")})
+@Tags({@Tag("end-to-end"), @Tag("inter-process"), @Tag("oracle")})
 
 public class AddEntandoPluginWithExternalOracleDatabaseIT extends AddEntandoPluginBaseIT {
 
