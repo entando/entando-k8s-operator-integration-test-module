f850f469b94e636938e1ab64c66b198f9df8877f
Kerruba <luca.cherubin@gmail.com>
2020-04-14 21:51:32 +0200

    ENG-322 Fix some issues with implementation

--- operator-common/src/test/java/org/entando/kubernetes/controller/common/examples/SampleController.java
+++ operator-common/src/test/java/org/entando/kubernetes/controller/common/examples/SampleController.java
@@ -32,8 +32,7 @@ import org.entando.kubernetes.controller.spi.Deployable;
 import org.entando.kubernetes.model.EntandoBaseCustomResource;
 import org.entando.kubernetes.model.EntandoDeploymentSpec;
 
-public class SampleController<T extends EntandoBaseCustomResource> extends AbstractDbAwareController<T> implements
-        StartupEventControllerObserver {
+public class SampleController<T extends EntandoBaseCustomResource> extends AbstractDbAwareController<T> {
 
     public SampleController(KubernetesClient kubernetesClient) {
         super(kubernetesClient, false);
@@ -43,7 +42,6 @@ public class SampleController<T extends EntandoBaseCustomResource> extends Abstr
         super(k8sClient, keycloakClient);
     }
 
-    @Override
     public void onStartup(StartupEvent event) {
         processCommand();
     }
