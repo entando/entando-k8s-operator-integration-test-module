912830e1018e7780ecae2078200b380b28c494ec
lucorset@gmail.com <burnl0nd0nburn!>
2020-06-29 10:45:01 +0200

    ENG-698 changed readyness endpoint to /entando-de-ap/api/health

---
+++ app-controller/src/test/java/org/entando/kubernetes/controller/app/EntandoAppDeployableContainerTest.java
@@ -0,0 +1,42 @@
+/*
+ *
+ * Copyright 2015-Present Entando Inc. (http://www.entando.com) All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ *  This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ */
+
+package org.entando.kubernetes.controller.app;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.entando.kubernetes.controller.common.KeycloakConnectionSecret;
+import org.entando.kubernetes.controller.inprocesstest.InProcessTestUtil;
+import org.entando.kubernetes.model.app.EntandoApp;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class EntandoAppDeployableContainerTest implements InProcessTestUtil {
+
+    private EntandoApp entandoApp = newTestEntandoApp();
+    @Mock
+    private KeycloakConnectionSecret keycloakConnectionSecret;
+
+    private EntandoAppDeployableContainer entandoAppDeployableContainer = new EntandoAppDeployableContainer(entandoApp, keycloakConnectionSecret);
+
+    @Test
+    void getHealthCheckPathTest() {
+
+        //noinspection OptionalGetWithoutIsPresent
+        assertEquals(EntandoAppDeployableContainer.INGRESS_WEB_CONTEXT + EntandoAppDeployableContainer.HEALTH_CHECK_PATH,
+                entandoAppDeployableContainer.getHealthCheckPath().get());
+    }
+}
