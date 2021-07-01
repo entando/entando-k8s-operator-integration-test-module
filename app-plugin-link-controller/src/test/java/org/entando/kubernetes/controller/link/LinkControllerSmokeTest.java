a20d82178ec1aa5fcf138de3ed49bec505815bbf
Ampie Barnard <ampie.private@gmail.com>
2021-07-01 13:23:05 +0200

    ENG-2092 more robust lazy initialization of crdnamemap

--- app-plugin-link-controller/src/test/java/org/entando/kubernetes/controller/link/LinkControllerSmokeTest.java
+++ app-plugin-link-controller/src/test/java/org/entando/kubernetes/controller/link/LinkControllerSmokeTest.java
@@ -103,7 +103,7 @@ class LinkControllerSmokeTest implements FluentIntegrationTesting {
             simpleClient.entandoResources()
                     .loadCustomResource(entandoApp.getApiVersion(), entandoApp.getKind(), entandoApp.getMetadata().getNamespace(),
                             entandoApp.getMetadata().getName());
-            startControllerFor(simpleClient, "entando-k8s-app-controller", this.entandoApp, "0.0.0-SNAPSHOT-PR-32-26");
+            startControllerFor(simpleClient, "entando-k8s-app-controller", this.entandoApp, "0.0.0-SNAPSHOT-PR-32-27");
             attachment("Entando App", objectMapper.writeValueAsString(this.entandoApp));
         });
         step("And I have created an EntandoPlugin custom resource", () -> {
@@ -123,7 +123,7 @@ class LinkControllerSmokeTest implements FluentIntegrationTesting {
                                     .endSpec()
                                     .build()
                     );
-            startControllerFor(simpleClient, "entando-k8s-plugin-controller", this.entandoPlugin, "0.0.0-SNAPSHOT-PR-27-11");
+            startControllerFor(simpleClient, "entando-k8s-plugin-controller", this.entandoPlugin, "0.0.0-SNAPSHOT-PR-27-12");
             attachment("Entando Plugin", objectMapper.writeValueAsString(this.entandoPlugin));
         });
         step("When I create an EntandoAppPluginLink to link the two custom resources", () -> {
