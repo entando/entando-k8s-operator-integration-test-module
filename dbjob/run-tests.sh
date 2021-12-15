5ff937445fd1320ab3461c8451587a98ded4b6e9
firegloves <lucorset@gmail.com>
2021-12-15 15:11:17 +0100

    ENG-2969 pipeline migration to github actions and project version bump to 7

--- dbjob/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-docker-compose -f docker-compose-cicd.yml build
-docker-compose -f docker-compose-cicd.yml up -d mysql postgresql
-docker-compose -f docker-compose-cicd.yml up entando-k8s-dbjob-test
-mkdir -p ./docker-cicd-result
-docker cp $(docker ps -aq --filter ancestor=entando-k8s-dbjob-test:latest):/usr/src/entando-k8s-dbjob/target ./docker-cicd-result/
-docker-compose -f docker-compose-cicd.yml down
